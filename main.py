9bb27b09ac31dd475ee2ed44eae21d626eab0b47
Mercsman <Tropicalxfury@gmail.com>
2025-07-26 13:38:46 -0700

    Added class and refresh rate

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from constants import *
 #####
 
 
+
+
 def main():
     pygame.init()
 
@@ -12,6 +14,8 @@ def main():
     print(f'Screen height: {SCREEN_HEIGHT}')
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -20,6 +24,8 @@ def main():
         screen.fill((0,0,0))
 
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
